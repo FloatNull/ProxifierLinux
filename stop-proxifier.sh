16db5d47a2ad90a01c3732dd9d42511ee30d3b88
StarRiver <desheng@lasolo.cn>
2024-12-10 14:10:43 +0800

    Update stop-proxifier.sh
    The original script severely affected the existing iptables rules.

--- stop-proxifier.sh
+++ stop-proxifier.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
-iptables -F
-iptables -Z
-iptables -X
-iptables -t nat -F
-iptables -t nat -Z
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -Z
-iptables -t mangle -X
+# del rules in REDSOCKS chain
+iptables -t nat -D REDSOCKS -d 0.0.0.0/8 -j RETURN
+iptables -t nat -D REDSOCKS -d 10.0.0.0/8 -j RETURN
+iptables -t nat -D REDSOCKS -d 127.0.0.0/8 -j RETURN
+iptables -t nat -D REDSOCKS -d 169.254.0.0/16 -j RETURN
+iptables -t nat -D REDSOCKS -d 172.16.0.0/16 -j RETURN
+iptables -t nat -D REDSOCKS -d 172.17.0.0/16 -j RETURN
+iptables -t nat -D REDSOCKS -d 192.168.0.0/16 -j RETURN
+iptables -t nat -D REDSOCKS -d 224.0.0.0/4 -j RETURN
+iptables -t nat -D REDSOCKS -d 240.0.0.0/4 -j RETURN
+iptables -t nat -D REDSOCKS -p tcp -j REDIRECT --to-ports 12345
+
+# del REDSOCKS related OUTPUT rules
+iptables -t nat -D OUTPUT -p tcp --dport 443 -j REDSOCKS
+iptables -t nat -D OUTPUT -p tcp --dport 80 -j REDSOCKS
+
+# del REDSOCKS related PREROUTING rules
+iptables -t nat -D PREROUTING -p tcp --dport 443 -j REDSOCKS
+iptables -t nat -t PREROUTING -p tcp --dport 80 -j REDSOCKS
+
+# del custom chain
+iptables -t nat -X REDSOCKS
+
 killall redsocks
-sudo fuser -k 12345/tcp
+fuser -k 12345/tcp
